146ece3e75acc60c8ebcbf41674312b88af9c38e
Souuhail <souhailharimau@gmail.com>
2024-11-06 20:00:25 +0100

    adding front in docker composes

--- src/main/java/tn/esprit/tpfoyer/control/EtudiantRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/EtudiantRestController.java
@@ -7,7 +7,6 @@ import tn.esprit.tpfoyer.service.IEtudiantService;
 
 import java.util.List;
 
-
 @RestController
 @AllArgsConstructor
 @RequestMapping("/etudiant")
@@ -15,56 +14,50 @@ public class EtudiantRestController {
 
     IEtudiantService etudiantService;
 
-
+    // Allow CORS from localhost:4200
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/retrieve-all-etudiants")
     public List<Etudiant> getEtudiants() {
         List<Etudiant> listEtudiants = etudiantService.retrieveAllEtudiants();
         return listEtudiants;
     }
 
-
-
-
-
-
-
-
-
-
-
-
+    // Allow CORS from localhost:4200
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/retrieve-etudiant-cin/{cin}")
     public Etudiant retrieveEtudiantParCin(@PathVariable("cin") Long cin) {
         Etudiant etudiant = etudiantService.recupererEtudiantParCin(cin);
         return etudiant;
     }
 
-
+    // Allow CORS from localhost:4200
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/retrieve-etudiant/{etudiant-id}")
     public Etudiant retrieveEtudiant(@PathVariable("etudiant-id") Long chId) {
         Etudiant etudiant = etudiantService.retrieveEtudiant(chId);
         return etudiant;
     }
 
-    // http://localhost:8089/tpfoyer/etudiant/add-etudiant
+    // Allow CORS from localhost:4200
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/add-etudiant")
     public Etudiant addEtudiant(@RequestBody Etudiant c) {
         Etudiant etudiant = etudiantService.addEtudiant(c);
         return etudiant;
     }
 
-    // http://localhost:8089/tpfoyer/etudiant/remove-etudiant/{etudiant-id}
+    // Allow CORS from localhost:4200
+    @CrossOrigin(origins = "http://localhost:4200")
     @DeleteMapping("/remove-etudiant/{etudiant-id}")
     public void removeEtudiant(@PathVariable("etudiant-id") Long chId) {
         etudiantService.removeEtudiant(chId);
     }
 
-    // http://localhost:8089/tpfoyer/etudiant/modify-etudiant
+    // Allow CORS from localhost:4200
+    @CrossOrigin(origins = "http://localhost:4200")
     @PutMapping("/modify-etudiant")
     public Etudiant modifyEtudiant(@RequestBody Etudiant c) {
         Etudiant etudiant = etudiantService.modifyEtudiant(c);
         return etudiant;
     }
-
-
 }
